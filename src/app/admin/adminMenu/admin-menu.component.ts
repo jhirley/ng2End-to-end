59d1aa259838919dcd15f538ed1e6c685320b171
Jhirley <jhirley.fonte@gmail.com>
2017-07-06 16:22:48 -0400

    checkin

---
+++ src/app/admin/adminMenu/admin-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   
+    templateUrl: './admin-menu.component.html',
+    styleUrls: ['./admin-menu.component.css']
+})
+
+export class AdminMenuComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/app/admin/adminShared/user.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable()
+export class UserService {
+
+    constructor() { }
+}
